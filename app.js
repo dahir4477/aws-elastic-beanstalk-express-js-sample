a114acc8a17ad1e08fc4ea9beaaa6e56cd2d3725
dahir4477 <dahir.niman@gmail.com>
2024-08-09 15:28:43 -0700

    Update app.js
    commitd buddy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, maxaad doonaysaa!'));
+app.get('/', (req, res) => res.send('Hello World, maxaad doonaysaa, halkani waa hargaysa oo idin leh soo dhawaada!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
